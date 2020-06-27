e45f0c8bcc5106c1df6ebedc7b2b0f66914df9cb
Miguel Pimentel <miguel.silva@tembici.com>
2020-06-27 10:00:11 -0300

    Initial commit

---
+++ Danger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Danger
+//
+//  Created by Miguel Pimentel on 27/06/20.
+//  Copyright © 2020 Miguel Pimentel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
