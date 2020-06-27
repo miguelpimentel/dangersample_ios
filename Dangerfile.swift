c87eebb6569d13334d26e04d1b27ee1700dd9181
Miguel Pimentel <miguel.silva@tembici.com>
2020-06-27 16:52:37 -0300

    Update Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,6 +1,9 @@
 import Danger 
 let danger = Danger()
 
+// MARK: - Swiftlint
+
+SwiftLint.lint(.all(directory: nil), inline: true)
 
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
